386c08f10f9434f210e3d58bc232db5d1571b205
Armin Irvije <armin.v2001@outlook.com>
2022-10-25 14:53:04 -0700

    Rough draft of Person class & testing git commands

---
+++ Person.cpp
@@ -0,0 +1,14 @@
+//author: Armin Irvije
+#include "Person.h"
+
+Person::Person(string person){
+
+        personName = person;
+
+    }
+
+string Person::getPerson(){
+
+    return personName;
+
+    }
---
+++ Person.h
@@ -0,0 +1,16 @@
+//author: Armin Irvije
+#include "common.h"
+
+class Person{
+
+private:
+
+    string personName;
+
+public: 
+
+    Person(string person);
+
+    string getPerson();
+
+};
